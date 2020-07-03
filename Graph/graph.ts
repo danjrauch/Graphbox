72e58db2c1bb0e963fced044768181aeb6759c6f
Dan Rauch <drauch@hawk.iit.edu>
2020-07-03 16:36:01 -0500

    index graph labels

--- Graph/graph.ts
+++ Graph/graph.ts
@@ -1,195 +1,188 @@
-enum EdgeType
-{
-    Undirected,
-    Directed,
+enum EdgeType {
+  Undirected,
+  Directed,
 }
 
-interface IVertex<T, S>
-{
-    id?: number;
-    label: T;
-    value: S;
+interface IVertex<TLabel, TValue> {
+  id?: number;
+  label: TLabel;
+  value: TValue;
 }
 
-interface IEdge<T>
-{
-    id?: number;
-    src: T;
-    dest: T;
-    weight?: number;
+interface IEdge<TLabel> {
+  id?: number;
+  src: TLabel;
+  dest: TLabel;
+  weight?: number;
 }
 
-function isVertex<T, S>(element: IVertex<T, S> | IEdge<T>): element is IVertex<T, S>
-{
-    return (<IVertex<T, S>>element).label !== undefined &&
-        (<IVertex<T, S>>element).value !== undefined;
+function isVertex<TLabel, TValue>(
+  element: IVertex<TLabel, TValue> | IEdge<TLabel>,
+): element is IVertex<TLabel, TValue> {
+  return (<IVertex<TLabel, TValue>> element).label !== undefined &&
+    (<IVertex<TLabel, TValue>> element).value !== undefined;
 }
 
-function isEdge<T, S>(element: IVertex<T, S> | IEdge<T>): element is IEdge<T>
-{
-    return (<IEdge<T>>element).src !== undefined &&
-        (<IEdge<T>>element).dest !== undefined;
+function isEdge<TLabel, TValue>(
+  element: IVertex<TLabel, TValue> | IEdge<TLabel>,
+): element is IEdge<TLabel> {
+  return (<IEdge<TLabel>> element).src !== undefined &&
+    (<IEdge<TLabel>> element).dest !== undefined;
 }
 
-interface IAdjacencyList
-{
-    [id: number]: number[];
+interface IAdjacencyList {
+  [id: number]: number[];
 }
 
-interface IAdjacencyMatrix
-{
-    [id: number]: number[];
+interface IAdjacencyMatrix {
+  [id: number]: number[];
 }
 
-interface IGraph<T, S>
-{
-    V: IVertex<T, S>[];
-    E: IEdge<T>[];
-    EdgeType: EdgeType;
-    add(element: IVertex<T, S>): void;
-    add(element: IEdge<T>): void;
-    removeVertex(label: T): void;
-    removeVerticies(value: S): void;
-    removeEdge(edge: IEdge<T>): void;
+interface IGraph<TLabel, TValue> {
+  V: IVertex<TLabel, TValue>[];
+  E: IEdge<TLabel>[];
+  EdgeType: EdgeType;
+  add(element: IVertex<TLabel, TValue>): void;
+  add(element: IEdge<TLabel>): void;
+  removeVertex(label: TLabel): void;
+  removeVerticies(value: TValue): void;
+  removeEdge(edge: IEdge<TLabel>): void;
 }
 
-class Graph<T, S> implements IGraph<T, S>
-{
-    private _vid: number = 0;
-    private _eid: number = -1;
+class Graph<TLabel, TValue> implements IGraph<TLabel, TValue> {
+  private _vid: number = 0;
+  private _eid: number = -1;
 
-    private _V: IVertex<T, S>[];
-    public get V(): IVertex<T, S>[]
-    {
-        return this._V;
+  private _V: IVertex<TLabel, TValue>[];
+  public get V(): IVertex<TLabel, TValue>[] {
+    return this._V;
+  }
+
+  private _VLabelIndex: Set<TLabel>;
+
+  private _E: IEdge<TLabel>[];
+  public get E(): IEdge<TLabel>[] {
+    return this._E;
+  }
+
+  private _edgeType: EdgeType;
+  public get EdgeType(): EdgeType {
+    return this._edgeType;
+  }
+
+  public get AdjacencyList(): IAdjacencyList {
+    let adjacencyList = this._V.reduce((prev, curr) => {
+      prev[curr.id!] = [];
+      return prev;
+    }, [] as IAdjacencyList);
+
+    this._E.forEach((e) => {
+      const srcId = this._V.filter((v) => v.label === e.src)[0].id!;
+      const destId = this._V.filter((v) => v.label === e.dest)[0].id!;
+
+      if (adjacencyList[srcId].indexOf(destId) === -1) {
+        adjacencyList[srcId].push(destId);
+      }
+      if (
+        this._edgeType === EdgeType.Undirected &&
+        adjacencyList[destId].indexOf(srcId) === -1
+      ) {
+        adjacencyList[destId].push(srcId);
+      }
+    });
+    return adjacencyList;
+  }
+
+  public get AdjacencyMatrix(): IAdjacencyMatrix {
+    let adjacencyMatrix = [] as IAdjacencyMatrix;
+    adjacencyMatrix = this._V.reduce((prev, curr) => {
+      prev[curr.id!] = [];
+      for (let i = 0; i < this._V.length; ++i) {
+        prev[curr.id!].push(0);
+      }
+      return prev;
+    }, [] as IAdjacencyMatrix);
+
+    this._E.forEach((e) => {
+      const srcId = this._V.filter((v) => v.label === e.src)[0].id!;
+      const destId = this._V.filter((v) => v.label === e.dest)[0].id!;
+
+      adjacencyMatrix[srcId][destId] = 1;
+      if (
+        this._edgeType === EdgeType.Undirected
+      ) {
+        adjacencyMatrix[destId][srcId] = 1;
+      }
+    });
+    return adjacencyMatrix;
+  }
+
+  public constructor();
+  public constructor(edgeType: EdgeType);
+  public constructor(
+    edgeType: EdgeType,
+    V: IVertex<TLabel, TValue>[],
+    E: IEdge<TLabel>[],
+  );
+  public constructor(
+    edgeType: EdgeType = EdgeType.Undirected,
+    V: IVertex<TLabel, TValue>[] = [],
+    E: IEdge<TLabel>[] = [],
+  ) {
+    this._V = V;
+    this._E = E;
+    this._edgeType = edgeType;
+    this._VLabelIndex = new Set(this._V.map((v) => v.label));
+  }
+
+  public add(element: IVertex<TLabel, TValue>): void;
+  public add(element: IEdge<TLabel>): void;
+  public add(element: IVertex<TLabel, TValue> | IEdge<TLabel>): void {
+    if (isVertex(element)) {
+      if (!this._VLabelIndex.has(element.label)) {
+        element.id = this._vid++;
+        this._V.push(element);
+        this._VLabelIndex.add(element.label);
+      }
+    } else {
+      if (
+        this._VLabelIndex.has(element.src) &&
+        this._VLabelIndex.has(element.dest)
+      ) {
+        element.id = this._eid--;
+        this._E.push(element);
+      }
     }
+  }
 
-    private _E: IEdge<T>[];
-    public get E(): IEdge<T>[]
-    {
-        return this._E;
+  public removeVertex(label: TLabel): void {
+    if (this._VLabelIndex.has(label)) {
+      this._V = this._V.filter((v) => v.label !== label);
+      this._VLabelIndex.delete(label);
     }
+  }
 
-    private _edgeType: EdgeType;
-    public get EdgeType(): EdgeType
-    {
-        return this._edgeType;
+  public removeVerticies(value: TValue): void {
+    const verticiesToRemove = this._V.filter((v) => v.value === value);
+    const labelsToRemove: Set<TLabel> = new Set<TLabel>(
+      verticiesToRemove.map((v) => v.label),
+    );
+    this._V = this._V.filter((v) => !labelsToRemove.has(v.label));
+    labelsToRemove.forEach((label) => this._VLabelIndex.delete(label));
+  }
+
+  public removeEdge(edge: IEdge<TLabel>): void {
+    if (this._edgeType === EdgeType.Undirected) {
+      this._E = this._E.filter((e) => {
+        (e.dest !== edge.dest || e.src !== edge.src) &&
+          (e.src !== edge.dest || e.dest !== edge.src);
+      });
+    } else if (this._edgeType === EdgeType.Directed) {
+      this._E = this._E.filter((e) =>
+        e.dest !== edge.dest || e.src !== edge.src
+      );
     }
-
-    private _adjacencyList: IAdjacencyList = [] as IAdjacencyList;
-    private _adjacencyMatrix: IAdjacencyMatrix = [] as IAdjacencyMatrix;
-
-    public constructor();
-    public constructor(edgeType: EdgeType);
-    public constructor(edgeType?: EdgeType)
-    {
-        if (typeof edgeType === "undefined")
-        {
-            this._V = [];
-            this._E = [];
-            this._edgeType = EdgeType.Undirected;
-        }
-        else
-        {
-            this._V = [];
-            this._E = [];
-            this._edgeType = edgeType;
-        }
-    }
-
-    private constructAdjacencyList(): void
-    {
-        this._adjacencyList = this._V.reduce((prev, curr) =>
-        {
-            prev[curr.id!] = [];
-            return prev;
-        }, [] as IAdjacencyList);
-
-        this._E.forEach((e) =>
-        {
-            const srcId = this._V.filter((v) => v.label === e.src)[0].id!;
-            const destId = this._V.filter((v) => v.label === e.dest)[0].id!;
-
-            if (this._adjacencyList[srcId].indexOf(destId) === -1)
-            {
-                this._adjacencyList[srcId].push(destId);
-            }
-            if (
-                this._edgeType === EdgeType.Undirected &&
-                this._adjacencyList[destId].indexOf(srcId) === -1
-            )
-            {
-                this._adjacencyList[destId].push(srcId);
-            }
-        });
-    }
-
-    private constructAdjacencyMatrix(): void
-    {
-        this._adjacencyMatrix = this._V.reduce((prev, curr) =>
-        {
-            prev[curr.id!] = [];
-            for (let i = 0; i < this._V.length; ++i)
-            {
-                prev[curr.id!].push(0);
-            } 
-            return prev;
-        }, [] as IAdjacencyMatrix);
-
-        this._E.forEach((e) =>
-        {
-            const srcId = this._V.filter((v) => v.label === e.src)[0].id!;
-            const destId = this._V.filter((v) => v.label === e.dest)[0].id!;
-
-            this._adjacencyMatrix[srcId][destId] = 1;
-            if (
-                this._edgeType === EdgeType.Undirected
-            )
-            {
-                this._adjacencyMatrix[destId][srcId] = 1;
-            }
-        });
-    }
-
-    public add(element: IVertex<T, S>): void;
-    public add(element: IEdge<T>): void;
-    public add(element: IVertex<T, S> | IEdge<T>): void
-    {
-        if (isVertex(element))
-        {
-            if (!this._V.some((e) => e.label === element.label))
-            {
-                element.id = this._vid++;
-                this._V.push(element);
-            }
-        }
-        else
-        {
-            if (
-                this._V.some((e) => e.label === element.src) &&
-                this._V.some((e) => e.label === element.dest)
-            )
-            {
-                element.id = this._eid++;
-                this._E.push(element);
-            }
-        }
-    }
-
-    public removeVertex(label: T): void
-    {
-        this._V = this._V.filter((v) => v.label !== label);
-    }
-
-    public removeVerticies(value: S): void
-    {
-        this._V = this._V.filter((v) => v.value !== value);
-    }
-
-    public removeEdge(edge: IEdge<T>): void
-    {
-        this._E = this._E.filter((e) => e.dest !== edge.dest || e.src !== edge.src);
-    }
+  }
 }
 
 export { EdgeType, Graph };
--- Graph/graph_test.ts
+++ Graph/graph_test.ts
@@ -1,15 +1,104 @@
 import { Graph, EdgeType } from "./graph.ts";
-import
-    {
-        assertEquals,
-        assertArrayContains,
-    } from "https://deno.land/std/testing/asserts.ts";
+import {
+  assertEquals,
+  assertArrayContains,
+} from "https://deno.land/std/testing/asserts.ts";
 
-Deno.test("graph simple add vertex", () => {
-    const g: Graph<string, string> = new Graph<string, string>(EdgeType.Undirected);
+function randomString(length: number): string {
+  let radom13chars = function () {
+    return Math.random().toString(16).substring(2, 15);
+  };
+  let loops = Math.ceil(length / 13);
+  return new Array(loops).fill(radom13chars).reduce((string, func) => {
+    return string + func();
+  }, "").substring(0, length);
+}
+
+Deno.test("g.add | add a simple vertex to graph", () => {
+  const g: Graph<string, string> = new Graph<string, string>(
+    EdgeType.Undirected,
+  );
+  g.add({
+    label: "A",
+    value: "Dan",
+  });
+  assertEquals(g.V, [{ id: 0, label: "A", value: "Dan" }]);
+});
+
+Deno.test("g.removeVertex | remove a simple vertex from graph", () => {
+  const g: Graph<string, string> = new Graph<string, string>(
+    EdgeType.Undirected,
+  );
+  g.add({
+    label: "A",
+    value: "Dan",
+  });
+  assertEquals(g.V, [{ id: 0, label: "A", value: "Dan" }]);
+  g.removeVertex("A");
+  assertEquals(g.V, []);
+});
+
+Deno.test("g.removeVerticies | remove a few simple verticies from graph", () => {
+  const g: Graph<string, number> = new Graph<string, number>(
+    EdgeType.Undirected,
+  );
+  g.add({
+    label: "A",
+    value: 0,
+  });
+  g.add({
+    label: "B",
+    value: 0,
+  });
+  assertEquals(
+    g.V,
+    [{ id: 0, label: "A", value: 0 }, { id: 1, label: "B", value: 0 }],
+  );
+  g.removeVerticies(0);
+  assertEquals(g.V, []);
+});
+
+Deno.test("g.removeEdge | remove a simple edge from graph", () => {
+  const g: Graph<string, number> = new Graph<string, number>(
+    EdgeType.Undirected,
+  );
+  g.add({
+    label: "A",
+    value: 0,
+  });
+  g.add({
+    label: "B",
+    value: 0,
+  });
+  g.add({
+    src: "A",
+    dest: "B",
+  });
+  assertEquals(
+    g.V,
+    [{ id: 0, label: "A", value: 0 }, { id: 1, label: "B", value: 0 }],
+  );
+  assertEquals(
+    g.E,
+    [{ id: -1, src: "A", dest: "B" }],
+  );
+  g.removeEdge({
+    src: "A",
+    dest: "B",
+  });
+  assertEquals(g.E, []);
+});
+
+Deno.test("g.add | add many simple verticies to graph", () => {
+  const g: Graph<string, number> = new Graph<string, number>(
+    EdgeType.Undirected,
+  );
+  const VSIZE = 10000;
+  for (let i = 0; i < VSIZE; ++i) {
     g.add({
-        label: "A",
-        value: "Dan",
+      label: randomString(30),
+      value: 0,
     });
-    assertEquals(g.V, [{ id: 0, label: "A", value: "Dan" }]);
+  }
+  assertEquals(g.V.length, VSIZE);
 });
